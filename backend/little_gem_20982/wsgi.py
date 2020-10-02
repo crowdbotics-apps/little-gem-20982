2888a825af8a3993b91050c635a1b591a3ca1616
Crowdbotics <team@crowdbotics.com>
2020-10-02 17:30:13 +0000

    Update models

--- backend/little_gem_20982/wsgi.py
+++ backend/little_gem_20982/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'little_gem_20982.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "little_gem_20982.settings")
 
 application = get_wsgi_application()
